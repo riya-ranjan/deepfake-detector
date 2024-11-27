8e42196e12f3f1c9f3beb242014183f7ab9fbc83
Riya Ranjan <riya.ranjan.00@gmail.com>
2024-11-27 15:28:42 -0800

    change logging behavior

--- train.py
+++ train.py
@@ -29,8 +29,8 @@ if __name__ == '__main__':
     )
     args = parser.parse_args()
     # Training parameters
-    batch_size = 1
-    learning_rate = 0.01
+    batch_size = 4
+    learning_rate = 0.1
     num_epochs = 10
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
