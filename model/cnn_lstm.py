aeb14c1a3118de13252da3ad2c7f4053ac532637
Riya Ranjan <riya.ranjan.00@gmail.com>
2024-11-30 00:56:42 -0800

    committing shit

--- model/cnn_lstm.py
+++ model/cnn_lstm.py
@@ -111,6 +111,6 @@ class Combined_CNN_LSTM(nn.Module):
         print("audio output")
         print(audio_output)
         # Concatenate the outputs from both branches
-        combined = torch.cat((video_output, audio_output))
+        combined = torch.cat((video_output, audio_output), dim=1)
         max_val = torch.max(combined)
         return max_val
