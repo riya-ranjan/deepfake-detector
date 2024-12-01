35c5335058b44d181908a03d972113841c8cef44
Riya Ranjan <riya.ranjan.00@gmail.com>
2024-11-30 23:38:58 -0800

    fixing shit

--- model/data_loader.py
+++ model/data_loader.py
@@ -12,7 +12,7 @@ class VideoDataset(Dataset):
         self.metadata = self._load_metadata(metadata_path)
         self.data_source = data_source
         self.video_files = [f for f in os.listdir(folder_path) if f.endswith('.mp4')]
-        video_files = video_files[5000]
+        self.video_files = self.video_files[0:5000]
         self.audio_length = audio_length
         self.max_frames = 512
         # We want the mel spectrogram to have 64 features
